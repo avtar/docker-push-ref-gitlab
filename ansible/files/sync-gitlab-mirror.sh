f19bd7d0f2a7ca89875baa3e8724e82503b65f84
Avtar Gill <avtar@ponderpad.localdomain>
2017-06-06 21:51:56 -0400

    Fails when invalid args provided

--- ansible/files/sync-gitlab-mirror.sh
+++ ansible/files/sync-gitlab-mirror.sh
@@ -1,5 +1,24 @@
 #!/bin/sh -e
 
+usage () { echo "Usage: $0 <ref> <repository>"; }
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Insufficient arguments provided!"
+    usage
+    exit 1
+fi
+
+for arg in "$@"; do
+    if [ ${#arg} -eq 0 ]; then
+        echo "Error: Argument cannot be an empty string!"
+        usage
+        exit 1
+    fi
+done
+
+REF="$1"
+REPO_NAME="$2"
+
 /usr/bin/push-ref-gitlab \
 --build-events-webhook-url="${BUILD_EVENTS_WEBHOOK_URL}" \
 --gitlab-instance="${GITLAB_INSTANCE_URL}" \
--- ansible/files/update-github-status.sh
+++ ansible/files/update-github-status.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -e
 
+usage () { echo "Usage: $0 <gitlab-state> <build-id> <sha> <build-stage> <repository>"; }
+
+if [ "$#" -ne 5 ]; then
+    echo "Error: Insufficient arguments provided!"
+    usage
+    exit 1
+fi
+
+for arg in "$@"; do
+    if [ ${#arg} -eq 0 ]; then
+        echo "Error: Argument cannot be an empty string!"
+        usage
+        exit 1
+    fi
+done
+
 GITLAB_STATE="$1"
 BUILD_ID="$2"
 SHA="$3"
@@ -8,14 +24,14 @@ REPOSITORY="$5"
 
 # Determine GitHub state
 if [ "${GITLAB_STATE}" = "created" ] || [ "${GITLAB_STATE}" = "running" ] || [ "${GITLAB_STATE}" = "pending" ]; then
-        GITHUB_STATE="pending"
+    GITHUB_STATE="pending"
 elif [ "${GITLAB_STATE}" = "success" ]; then
-        GITHUB_STATE="success"
+    GITHUB_STATE="success"
 elif [ "${GITLAB_STATE}" = "failed" ]; then
-        GITHUB_STATE="failure"
+    GITHUB_STATE="failure"
 else
-        echo "Unknown GitLab state: ${GITLAB_STATE}"
-        exit 1
+    echo "Unknown GitLab state: ${GITLAB_STATE}"
+    exit 1
 fi
 
 BUILD_URL="${GITLAB_INSTANCE_URL}"/"${GITLAB_ACCOUNT}"/"${REPOSITORY}"/builds/"${BUILD_ID}"
